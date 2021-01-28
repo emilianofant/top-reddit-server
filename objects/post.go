7caeaad84ff7578d7e9234e30db133d71cf6a584
Emiliano Fant <emilianofant@github.com>
2021-01-28 11:47:58 -0300

    fix: post json naming

--- objects/post.go
+++ objects/post.go
@@ -6,7 +6,7 @@ type Post struct {
 	Title            string  `json:"title,omitempty"`
 	Author           string  `json:"author,omitempty"`
 	EntryDate        float64 `json:"created_utc,omitempty"`
-	Thumbnail        string  `json:"thumbnail,omitempty"`
+	Thumbnail        string  `json:"thumbnail"`
 	PostURL          string  `json:"permalink,omitempty"`
 	NumberOfComments int     `json:"num_comments,omitempty"`
 	Status           bool    `json:"status"`
